a4c5728eee079167f0b0dd4e81b0c185c68a6039
Papino7 <sowp82@gmail.com>
2024-10-14 22:17:11 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,14 +4,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "48459439078"
-global.ownername = "Toxxic Md"
-global.ytname = "Toxxic Md"
+global.ownernomer = "221762359904"
+global.ownername = "{*Pāpīñø_Lē_Kīñg🤴*}"
+global.ytname = "{*Pāpīñø_Lē_Kīñg🤴*"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '48459439078'  //creator number
-global.ownername = 'Toxxic Md' //owner name
+global.ownernumber = '221762359904'  //creator number
+global.ownername = 'Mouhamed Gabriel' //owner name
 global.botname = 'ᴢᴇɴᴏɴ ᴍᴅ' //name of the bot
 
 //sticker details
